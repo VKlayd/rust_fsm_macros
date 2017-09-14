cf32d0c6409c34f6dd18409d87ad16d8444b4792
Victor Korkin <victor.korkin@gmail.com>
2017-09-14 17:26:59 +0700

    Add context and in/out callbacks.
    Add local context for states. Each state has own context that created when Machine enter into this state and destroyed when leave state.
    Callbacks can access state context and change it.
    
    Add callbacks on enter and leave each state.

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,60 @@
 macro_rules! FSM {
 (
-    name: $machine:ident (init: $initial:ident );
+    name: $machine:ident (init: $initial:ident{$($init_field:ident:$init_val:expr),*});
     states:($($states:ident),*);
     commands:($($commands:ident),*);
 
-    $(($state:ident:
-        $({$cmd:ident;$callback:stmt;$new_state:ident}),*
+    $(($state:ident {$($el:ident:$typ:ty),*} /$sel:ident/:
+        >> $income:block,
+        << $outcome:block,
+        $({$cmd:ident;$callback:block;$new_state:ident{$($new_el:ident:$new_el_val:expr),*}}),*
     )),*
 ) => (
     mod $machine {
+        trait CanDoJob {
+            fn do_job(&mut self, cmd: &Commands) -> Option<States>;
+            fn leave(&self) -> Result<(), ()>;
+            fn enter(&self) -> Result<(), ()>;
+        }
+
+        $(
         #[derive(Debug)]
+        #[derive(PartialEq)]
+        struct $state{$($el:$typ),*}
+
+        impl CanDoJob for $state {
+            fn do_job(&mut self, cmd: & Commands) -> Option<States> {
+                let $sel = self;
+                match *cmd {
+                    $(Commands::$cmd => {
+                        $callback;
+                        Some(States::$new_state{context: $new_state{$($new_el:$new_el_val),*}})
+                    })*
+                    _ => None
+                }
+            }
+            fn leave(&self) -> Result<(), ()> {
+                let $sel = self;
+                $outcome
+                Ok(())
+            }
+            fn enter(&self) -> Result<(), ()> {
+                let $sel = self;
+                $income
+                Ok(())
+            }
+        }
+        )*
+
+
+        #[derive(Debug)]
+        #[derive(PartialEq)]
         pub enum States {
-            $($states),*
+            $($states {context: $states}),*
         }
 
         #[derive(Debug)]
+        #[derive(PartialEq)]
         pub enum Commands {
             $($commands),*
         }
@@ -23,38 +63,34 @@ macro_rules! FSM {
             state: States
         }
         pub fn new() -> Machine {
-            Machine {state: States::$initial}
+            let context = $initial{$($init_field: $init_val),*};
+            context.enter().unwrap();
+            Machine{state: States::$initial{context: context}}
         }
-
-        trait CanDoJob {
-            fn do_job(cmd: & Commands) -> Option<States>;
-        }
-
-        $(
-        struct $state{}
-        impl CanDoJob for $state {
-            fn do_job(cmd: & Commands) -> Option<States> {
-                match *cmd {
-                    $(Commands::$cmd => {
-                        $callback;
-                        Some(States::$new_state)
-                    })*
-                    _ => None
-                }
-            }
-        }
-        )*
 
         impl Machine {
             pub fn execute(&mut self, cmd: & Commands) {
                 match {
                     match self.state {
-                        $(States::$states => $states::do_job(cmd)),*
+                        $(States::$states{ ref mut context } => context.do_job(cmd)),*
                     }
                 } {
-                    Some(x) => {println!("{:?}: {:?} -> {:?}", cmd, self.state, x); self.state = x},
+                    Some(x) => {
+                        if x != self.state {
+                            self.change_state(x)
+                        }
+                    },
                     None => println!("Wrong operation {:?} for {:?} state!", cmd, self.state)
                 }
+            }
+            fn change_state(&mut self, new_state: States) {
+                match self.state {
+                    $(States::$states{ ref mut context } => context.leave()),*
+                }.unwrap();
+                self.state = new_state;
+                match self.state {
+                    $(States::$states{ ref mut context } => context.enter()),*
+                }.unwrap();
             }
         }
     }
@@ -65,19 +101,25 @@ macro_rules! FSM {
 mod tests {
 
     FSM!(
-    name:Mach1(init:New);
+    name:Mach1 (init: New{x:0});
     states:(New, InConfig, Operational);
-    commands:(Configure, ConfigureDone);
+    commands:(Configure, ConfigureDone, Drop);
 
-    ( New:
-        {Configure;     {println!("In New. Cmd Configure. Send InConfig.");};             InConfig},
-        {ConfigureDone; {println!("In New. Cmd ConfigureDone. Send InConfig.");};         New}
+    ( New {x: i16} /context/:
+        >> {println!("Enter {:?}", context)},
+        << {println!("Leave {:?}", context)},
+        {Configure;     {println!("In New with context val: {}", context.x);};     InConfig{x:context.x+1, y:0}},
+        {ConfigureDone; {};   New{x:0}}
     ),
-    ( InConfig:
-        {ConfigureDone; {println!("In InConfig. Cmd ConfigureDone. Send Operational.");}; Operational}
+    ( InConfig {x:i16, y:i16} /context/:
+        >> {println!("Enter {:?}", context)},
+        << {println!("Leave {:?}", context)},
+        {ConfigureDone; {}; Operational{x:context.x+1, y:context.y+1}}
     ),
-    ( Operational:
-
+    ( Operational {x:i16, y:i16} /context/:
+        >> {println!("Enter {:?}", context)},
+        << {println!("Leave {:?}", context)},
+        {Drop; {}; New{x:context.x+1}}
     )
     );
 
@@ -87,5 +129,8 @@ mod tests {
         m.execute(&Mach1::Commands::Configure);
         m.execute(&Mach1::Commands::ConfigureDone);
         m.execute(&Mach1::Commands::Configure);
+        m.execute(&Mach1::Commands::Drop);
+        m.execute(&Mach1::Commands::Configure);
+        m.execute(&Mach1::Commands::ConfigureDone);
     }
 }
